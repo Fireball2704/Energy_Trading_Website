1f3ddf2d051a43f4c3a6490f9823376af2498295
Ruvina <vasruvina@gmail.com>
2025-03-14 19:33:23 +0530

    Added .gitkeep to track public/img folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./components/Main"; //Home
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/buyer_dashboard/b_dashboard" element={<buyer_dashboard />} />
+        <Route path="/" element={<><Header /><Main /><Footer /></>} />
+        <Route path="/register" element={<><Register /><Footer /></>} />
+        <Route path="/login" element={<><Login /><Footer /></>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  </Router>
   );
-}
+} 
 
 export default App;
